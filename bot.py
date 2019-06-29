a93323dcff16dac9dea40feee19e435283c61ae2
0xh4sh <0xh4sh@protonmail.com>
2019-06-29 17:31:57 +0200

    Fixing more dumb shit.

--- bot.py
+++ bot.py
@@ -9,11 +9,11 @@ Token = open("Token.txt", 'r').read()
 
 Categories = ['femdom', 'tickle', 'classic', 'ngif', 'erofeet', 'meow', 'erok', 'poke', 'les', 'v3', 'hololewd', 'nekoapi_v3.1', 'lewdk', 'keta', 'feetg', 'nsfw_neko_gif', 'eroyuri', 'kiss', '8ball', 'kuni', 'tits', 'pussy_jpg', 'cum_jpg', 'pussy', 'lewdkemo', 'lizard', 'slap', 'lewd', 'cum', 'cuddle', 'spank', 'smallboobs', 'goose', 'Random_hentai_gif', 'avatar', 'fox_girl', 'nsfw_avatar', 'hug', 'gecg', 'boobs', 'pat', 'feet', 'smug', 'kemonomimi', 'solog', 'holo', 'wallpaper', 'bj', 'woof', 'yuri', 'trap', 'anal', 'baka', 'blowjob', 'holoero', 'feed', 'neko', 'gasm', 'hentai', 'futanari', 'ero', 'solo', 'waifu', 'pwankg', 'eron', 'erokemo']
 
-SendCategories = "\n".join(str(x) for x in Categories)
+SendCategories = "\n".join(Categories)
 
 def GetURL(BaseUrl):
     resp = requests.get(BaseUrl)
-    URL = resp.json["url"]
+    URL = resp.json()["url"]
 
     return(URL)
 
